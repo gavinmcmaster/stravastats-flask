b46badd2b088e21de28270ffc3a2dc48a497ff2e
Gavin McMaster <gavmcmaster@gmail.com>
2022-01-10 21:19:13 +0000

    Tests: Add activity routes

---
+++ src/tests/test_activity.py
@@ -0,0 +1,53 @@
+from stravastats.db import get_db
+from stravastats.api.models import Activity
+from sqlalchemy import insert
+
+
+def test_add_activity(app, client):
+    body = {
+        "athlete_id": 4009189,
+        "name": "NC500: Brora to Dingwall",
+        "distance": 107254,
+        "moving_time": 19701,
+        "elapsed_time": 27556,
+        "elevation_gain": 750,
+        "start_date": 1640013750,
+        "start_latitude": 58.012933,
+        "start_longitude": -4.425155,
+        "achievement_count": 0,
+        "kudos_count": 18,
+        "comment_count": 2,
+        "photo_count": 0,
+        "description": "It doesn't take much of a hill to put me in the small ring now",
+        "calories": 2884,
+        "average_speed": 5.444,
+        "max_speed": 14.5,
+        "pr_count": 0,
+        "average_temp": 12,
+        "bikepacking": 1
+    }
+
+    response = client.post(
+        "/activity/add", json=body
+    )
+    assert response.status_code == 201
+
+    with app.app_context():
+        db = get_db()
+        count = db.session.query(Activity.id).count()
+        assert count == 1
+
+
+def test_get_activity(app, client):
+    with app.app_context():
+        db = get_db()
+        stmt = insert(Activity).values(athlete_id=4009176, name="NC500: Brora to Dingwall",
+                                       distance=107254, moving_time=19701, elapsed_time=27556, elevation_gain=750,
+                                       start_date=1640013750, kudos_count=18, comment_count=2, photo_count=0,
+                                       description="It doesn't take much of a hill to put me in the small ring now",
+                                       calories=2884, average_speed=5.444, max_speed=14.5, pr_count=0, average_temp=12)
+        with db.engine.connect() as conn:
+            result = conn.execute(stmt)
+
+        response = client.get("/activities/4009176")
+        assert response.status_code == 200
---
+++ src/tests/test_athlete.py
@@ -0,0 +1,44 @@
+from stravastats.db import get_db
+from stravastats.api.models import Athlete
+from sqlalchemy import insert
+
+
+def test_add_athlete(app, client):
+    body = {
+        "strava_id": "4009189",
+        "username": "testuser",
+        "firstname": "Test",
+        "lastname": "User",
+        "city": "Brighton",
+        "country": "UK",
+        "email": "testuser1@inavlid.com",
+        "sex": "M",
+        "weight": 75,
+        "profile": "https://dgalywyr863hv.cloudfront.net/pictures/athletes/4009189/2904544/2/large.jpg",
+        "created_at": 1639765803
+    }
+
+    response = client.post(
+        "/athlete/add", json=body
+    )
+    assert response.status_code == 201
+
+    with app.app_context():
+        db = get_db()
+        count = db.session.query(Athlete.id).count()
+        assert count == 1
+
+
+def test_get_athlete(app, client):
+    with app.app_context():
+        db = get_db()
+        stmt = insert(Athlete).values(
+            strava_id='7355260', username="test", firstname="Test",
+            lastname="User 1", city="London", country="UK", email="test@invalid.com",
+            sex="F", weight=100, profile="https://testurl.com/test.jpg",
+            created_at=1639765803, updated_at=1639765803)
+        with db.engine.connect() as conn:
+            result = conn.execute(stmt)
+
+    response = client.get("/athlete/1")
+    assert response.status_code == 200
