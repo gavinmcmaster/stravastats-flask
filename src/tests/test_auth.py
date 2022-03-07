7acd063c7260bb7677a4d9d53920b3bc9eadb082
Gavin McMaster <gavmcmaster@gmail.com>
2022-03-07 21:20:11 +0000

    Tests: Add logout route

--- src/tests/test_auth.py
+++ src/tests/test_auth.py
@@ -56,3 +56,41 @@ def test_unregistered_user_login_attempt(app, client):
     assert data['status'] == 'fail'
     assert response.content_type == 'application/json'
     assert response.status_code == 401
+
+
+def test_logout(app, client):
+    with app.app_context():
+        db = get_db()
+        user = User(
+            email='gavin@invalid.com',
+            password='123456'
+        )
+        db.session.add(user)
+        db.session.commit()
+
+        # user login
+        login_response = client.post(
+            '/auth/login',
+            data=json.dumps(dict(
+                email='gavin@invalid.com',
+                password='123456'
+            )),
+            content_type='application/json'
+        )
+        login_data = json.loads(login_response.data.decode())
+        assert login_data['message'] == 'User successfully logged in'
+        assert login_data['auth_token']
+        assert login_data['status'] == 'success'
+        assert login_response.content_type == 'application/json'
+        assert login_response.status_code == 200
+
+        response = client.post(
+            '/auth/logout',
+            headers=dict(
+                Authorization='Bearer ' + login_data['auth_token']
+            )
+        )
+        data = json.loads(response.data.decode())
+        assert response.status_code, 200
+        assert data['status'] == 'success'
+        assert data['message'] == 'Successfully logged out'
