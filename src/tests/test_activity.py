4073b31c4a84de7d5f1fb6a1c658e0e7a8d64189
Gavin McMaster <gavmcmaster@gmail.com>
2022-03-14 22:05:33 +0000

    Tests: Add activity test

--- src/tests/test_activity.py
+++ src/tests/test_activity.py
@@ -61,3 +61,23 @@ def test_get_activity(app, client, login):
                                   Authorization='Bearer ' + login['auth_token']
                               ))
         assert response.status_code == 200
+
+
+def test_get__no_activities(app, client, login):
+    with app.app_context():
+        db = get_db()
+        stmt = insert(Activity).values(athlete_id=5009176, name="NC500: Brora to Dingwall",
+                                       distance=107254, moving_time=19701, elapsed_time=27556, elevation_gain=750,
+                                       start_date=1640013750, kudos_count=18, comment_count=2, photo_count=0,
+                                       description="It doesn't take much of a hill to put me in the small ring now",
+                                       calories=2884, average_speed=5.444, max_speed=14.5, pr_count=0, average_temp=12)
+        with db.engine.connect() as conn:
+            result = conn.execute(stmt)
+
+        response = client.get("/activities/4009176",
+                              headers=dict(
+                                  Authorization='Bearer ' + login['auth_token']
+                              ))
+        data = json.loads(response.data.decode())
+        assert data['message'] == 'No activities found for this athlete'
+        assert response.status_code == 400
